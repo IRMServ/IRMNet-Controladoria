bab72e0cec10953a3ca1cf6ffda87a542d914957
IgorDePaula <principe.borodin@gmail.com>
2013-06-04 11:59:16 -0300

    Inclusao das acl

--- config/module.config.php
+++ config/module.config.php
@@ -1,5 +1,5 @@
 <?php
-
+namespace Controladoria;
 /**
  * Zend Framework (http://framework.zend.com/)
  *
@@ -8,6 +8,16 @@
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 return array(
+      'acl' => array(
+        'Controladoria' => array(
+            'CONTROLADORIA' => array(
+                'Controladoria\Controller\Index:index',              
+            ),
+            'TI' => array(
+                'Controladoria\Controller\Index:index',              
+            ),
+        )
+    ),
     'router' => array(
         'routes' => array(
             // This defines the hostname route which forms the base
